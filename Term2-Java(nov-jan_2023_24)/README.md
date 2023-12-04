fef1c74c403a2b29bbd7fb77d4a59e791beb8eac
Ada <obenedicta4.@gmail.com>
2023-12-04 22:20:03 +0100

    UpdatingcodeandReade

--- Term2-Java(nov-jan_2023_24)/README.md
+++ Term2-Java(nov-jan_2023_24)/README.md
@@ -1,4 +1,103 @@
 # Term 2 year 1
-- Quiz
-- Library system
-- Student record management system
+# Quiz
+# Library system
+# Student record management system
+### Student Record Management System Documentation
+
+#### Overview:
+
+This Java program implements a Student Record Management System for a university. It allows administrators to add new students, update student information, and view student details.
+
+#### Classes:
+
+1. **Student Class:**
+    - `Student`: Represents a student with private instance variables for name, ID, age, and grade. Includes a constructor and getters/setters.
+
+2. **Student Management Class:**
+    - `StudentManagement`: Manages the list of students and provides methods for adding, updating, and viewing student information.
+
+#### How to Run:
+
+1. **Compile the Program:**
+    - Save the provided code in a file named `Main.java`.
+    - Open a terminal and navigate to the directory containing the file.
+    - Compile the program using the command: `javac Main.java`
+
+2. **Run the Program:**
+    - Run the compiled program with the command: `java Main`
+
+#### Administrator Interface:
+
+Upon running the program, the administrator is presented with a menu:
+
+```
+1. Add a new student
+2. Update student information
+3. View student details
+4. Exit
+```
+
+#### Interacting with the Interface:
+
+1. **Add a New Student:**
+    - Choose option 1.
+    - Enter the student's name, ID, age, and grade when prompted.
+
+2. **Update Student Information:**
+    - Choose option 2.
+    - Enter the student ID for updating.
+    - Provide the new age and grade.
+
+3. **View Student Details:**
+    - Choose option 3.
+    - Enter the student ID to view details.
+
+4. **Exit:**
+    - Choose option 4 to exit the program.
+
+#### Error Handling:
+
+- The program handles cases where the provided student ID is not found.
+- Invalid inputs (non-numeric values, etc.) are not handled in this basic implementation and may cause exceptions.
+
+#### Example Interaction:
+
+```
+1. Add a new student
+2. Update student information
+3. View student details
+4. Exit
+
+Enter your choice: 1
+Enter student name: John Doe
+Enter student ID: 123
+Enter student age: 20
+Enter student grade: 85.5
+Student added successfully.
+
+1. Add a new student
+2. Update student information
+3. View student details
+4. Exit
+
+Enter your choice: 3
+Enter student ID: 123
+Student Details:
+Name: John Doe
+ID: 123
+Age: 20
+Grade: 85.5
+
+1. Add a new student
+2. Update student information
+3. View student details
+4. Exit
+
+Enter your choice: 2
+Enter student ID for update: 123
+Enter new age: 21
+Enter new grade: 90.0
+Student information updated successfully.
+```
+
+Feel free to enhance the program based on specific requirements and additional error-handling scenarios.
--- Term2-Java(nov-jan_2023_24)/StuRecordMgtSystem.java
+++ Term2-Java(nov-jan_2023_24)/StuRecordMgtSystem.java
@@ -3,33 +3,36 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 
+/**
+ * StuRecordMgtSystem - Entry point 
+ */
 public class StuRecordMgtSystem {
     // Administrator Interface
     private static Scanner scan = new Scanner(System.in);// to get users input
-    // Array to store students
-    private static ArrayList<Student> studentsList = new ArrayList<>();
+
     public static void main(String[] args){
         boolean exit = false;
         //creating an infinte loop
         while (!exit) {
-            System.out.println("\nChoose from the Menu: new, update, view or exit:");
+
+            System.out.println("\nWELCOME!!! Administrator");
             System.out.println("new - To add new student.");
-            System.out.println("update - To update student.");
+            System.out.println("upd - To update student.");
             System.out.println("view - To student details.");
-            System.out.println("exit - To exit.\n");
+            System.out.println("ext - To exit.\n");
 
             String option = scan.nextLine();
             switch (option) {
                 case "new":
-                    newStudent();
+                    StudentManagement.newStudent();
                     break;
-                case "update":
-                    updateStudentInfo();
+                case "upd":
+                    StudentManagement.updateStudentInfo();
                     break;
-                // case "view":
-                //     viewStudentDetails();
-                //     break;
-                case "exit":
+                case "view":
+                    StudentManagement.viewStudentDetails();
+                    break;
+                case "ext":
                     exit = true;
                     scan.close();
                     System.out.println("\nExiting...");
@@ -40,16 +43,29 @@ public class StuRecordMgtSystem {
             }
         } 
     }
+}
 
+/**
+ * StudentManagement: add, upload and view
+ */
+class StudentManagement {
+    private static Scanner scan = new Scanner(System.in);// to get users input
+
+    // Array to store students
+    private static ArrayList<Student> studentsList = new ArrayList<>();
+    private static int numberOfStudents = 0;
 
     // adding new student
-    private static void newStudent(){
-        System.out.println("\nADDING NEW STUDENT");
+    public static void newStudent(){
+        System.out.println("\nADDING NEW STUDENT\n");
 
         try{
             //enter student id
-            System.out.println("Student ID: ");
+            System.out.print("Student ID: ");
             int ID = scan.nextInt();
+            if(ID < 1){
+                throw new NonNegativeNumber("ID should be positive digit.");
+            }
             scan.nextLine();
 
             // confirm if student id already existing
@@ -61,31 +77,40 @@ public class StuRecordMgtSystem {
                 System.out.println("Student with ID " + ID + " already exist.");
                 System.out.println("If you intend to update then choose the update option.");
             } else {
+                numberOfStudents++;
+
                 // get the name
-                System.out.println("Student name: ");
+                System.out.print("Student name: ");
                 String name = scan.nextLine();
 
                 // get the age
-                System.out.println("Student age: ");
+                System.out.print("Student age: ");
                 int age = scan.nextInt();
+                if(age < 1){
+                    throw new NonNegativeNumber("Age should be a positive digit.");
+                }
                 scan.nextLine();
 
                 // get the grade
-                System.out.println("Student grade: ");
+                System.out.print("Student grade: ");
                 float grade = scan.nextFloat();
+                if(grade < 0.0){
+                    throw new NonNegativeNumber("Grade should be positive. E.g 4.0");
+                }
                 scan.nextLine();
 
                 // create new instance of student
                 Student new_stu = new Student(name, ID, age, grade);
                 studentsList.add(new_stu);
-                System.out.println("New student details has been added");
+                System.out.println(numberOfStudents + " New student details has been added");
             }
+        } catch(NonNegativeNumber e){
+            System.out.println(e.getMessage());
+            scan.nextLine();
         } catch(InputMismatchException e){
-                System.out.println("Error:Wrong input ");
-                System.out.println("for age and ID, enter a positive digit. For grade use decimal point e.g. 4.0");
+            System.out.println("Error:Wrong input:\n" + e);
+            scan.nextLine();
         } catch(Exception e){
-            // if (e) handle input mismact exept,
-            //     System.out.println("Enter positive value.");
                 System.out.println("Error:Something broke " + e);
                 scan.nextLine();
         }
@@ -93,63 +118,70 @@ public class StuRecordMgtSystem {
 
 
     // update student information
-    private static void updateStudentInfo(){
+    public static void updateStudentInfo(){
         System.out.println("\nUPDATING STUDENTS' INFORMATION");
             
         //enter student id
-        System.out.println("ID of the student you want to update: ");
+        System.out.print("ID of the student you want to update: ");
         int ID = scan.nextInt();
         scan.nextLine();
 
         // confirm if student (id) exists in the list
         Student exist = checkID(ID);
 
-        try {
-            if (exist != null) {
-                // find what is to be updated
-                System.out.println("\nEnter 1 - to update ID");
-                System.out.println("\nEnter 2 - to update name");
-                System.out.println("\nEnter 3 - to update age");
-                System.out.println("\nEnter 4 - to update grade");
-                System.out.println("\nEnter 0 - to exit");
-                int option = scan.nextInt();
-                scan.nextLine();
+        if (exist != null) {
+            boolean exit = false;
+            try {
                 
-                while(option!=0){
+                while(!exit){
+                    // find what is to be updated
+                    System.out.println("Enter 1 - to update ID");
+                    System.out.println("Enter 2 - to update name");
+                    System.out.println("Enter 3 - to update age");
+                    System.out.println("Enter 4 - to update grade");
+                    System.out.println("Enter 0 - Back to menu");
+                    int option = scan.nextInt();
+                    scan.nextLine();
+
                     switch(option){
                         case 1:
                             // update the id
-                            System.out.println("Update student ID: ");
+                            System.out.print("Update student ID: ");
                             int up_id = scan.nextInt();
                             updateID(exist, up_id);
                             break;
                         case 2:
                             // update the name
-                            System.out.println("Update student name: ");
-                            String up_name = scan.nextLine();
-                            exist.setName(up_name);
+                            System.out.print("Update student name: ");
+                            String name = scan.nextLine();
+                            exist.setName(name);
                             break;
                         case 3:
                             // set the age
-                            System.out.println("Student age: ");
+                            System.out.print("Student age: ");
                             int up_age = scan.nextInt();
                             updateAge(exist, up_age);
                             break;
                         case 4:
                             // set the grade
-                            System.out.println("Student grade: ");
+                            System.out.print("Student grade: ");
                             float up_grade = scan.nextFloat();
                             updateGrade(exist, up_grade);
+                            break;
+                        case 0:
+                            exit = true;
+                            System.out.println("Exiting...");
                             break;
                         default:
                             System.out.println("NOT AN OPTION");
                             break;
                     }
+                    System.out.println("Students' details successfully updated");
                 }
-                
-                System.out.println("Students' details successfully updated");
-            }
-        } catch (Exception e) {// handle input mismach exception
+            } catch (InputMismatchException e) {
+                System.out.println("Error:Wrong input type");
+                scan.nextLine();
+            } catch (Exception e) {
             // TODO: handle exception
             if (ID != (int) ID){ // if id is not type interger
                 System.out.println("ID " + ID + " should be digits.");
@@ -159,6 +191,9 @@ public class StuRecordMgtSystem {
             }else{
                 System.out.println("Error: Something went wrong " + e);
             }
+            }
+        }else{
+            System.out.println(ID + " does not exist\nExiting...");
         }
     }
 
@@ -192,6 +227,63 @@ public class StuRecordMgtSystem {
     }
 
 
+    // view the student table
+    public static void viewStudentDetails(){
+        boolean ext = false;
+
+        System.out.println("\nVIEW STUDENTS' INFORMATION");
+            
+        while(!ext){
+            // find what is to be updated
+            System.out.println("Enter 1 - to view a student record");
+            System.out.println("Enter all - to view the entire list");
+            System.out.println("Enter 0 - exit from VIEW\n");
+            String option = scan.nextLine();
+
+            switch(option){
+                case "1":
+                    // view by id
+                    System.out.print("Student ID: ");
+                    int ID = scan.nextInt();
+                    scan.nextLine();
+
+                    // confirm if student (id) exists in the list
+                    Student exist = checkID(ID);
+
+                    if (exist != null) {
+                        //list header
+                        System.out.printf("%-15s%-15s%-15s%-15s\n","ID", "Name", "Age", "Grade");
+                        System.out.println("--------------------------------------------");
+
+                        // list body
+                        System.out.printf("%-15d%-15s%-15d%-15.2f\n", exist.returnId(), exist.returnName(), exist.returnAge(), exist.returnGrade()); 
+                    }else{
+                        System.out.println("Student with ID " + ID + " does not exist");
+                    }                     
+                    break;
+                case "all":
+                    // view all
+                    //list header
+                    System.out.println("\nTotal Number of Students: " + StudentManagement.numberOfStudents);
+                    System.out.printf("%-15s%-15s%-15s%-15s\n","ID", "Name", "Age", "Grade");
+                    System.out.println("---------------");            
+
+                    // list body
+                    for (Student s : studentsList) {
+                        System.out.printf("%-15d%-15s%-15d%-15.2f\n", s.returnId(), s.returnName(), s.returnAge(), s.returnGrade()); 
+                    }
+                    break;
+                case "0":
+                    ext = true;
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("NOT AN OPTION");
+                    break;
+            }
+        }
+    }
+
     // check if id already exist
     private static Student checkID(int id){
         for (Student stu_id : studentsList) {
@@ -205,7 +297,10 @@ public class StuRecordMgtSystem {
 }
 
 
-// student class with private instance variables for storing student information such as name, ID, age, and grade.
+/**
+ * Student: with private instance variables for storing
+ * student information such as name, ID, age, and grade.
+ */
 class Student{
     private String name;
     private int ID;
@@ -244,10 +339,29 @@ class Student{
     public int returnId(){
         return this.ID;
     }
+
+    // return name demanded
+    public String returnName(){
+        return this.name;
+    }
+
+    // return age demanded
+    public int returnAge(){
+        return this.age;
+    }
+
+    // return grade demanded
+    public float returnGrade(){
+        return this.grade;
+    }
 }
 
-class StudentManagement {
-    private static String[] listOfStudent;
-    private static int numberOfStudents;
-}
 
+/**
+ * NonNegativeNumber: Custom Exception
+ */
+public class NonNegativeNumber extends Exception{
+    public NonNegativeNumber(String message){
+        super(message);
+    }
+}
