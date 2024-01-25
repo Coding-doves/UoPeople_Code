209124f95470afadacf51b3a7dc80d89e8a56d3f
Ada <obenedicta4.@gmail.com>
2024-01-25 12:20:41 +0100

    fixed

--- Term2-Java(nov-jan_2023_24)/CourseAndGradeManagement/src/app/CourseManagement.java
+++ Term2-Java(nov-jan_2023_24)/CourseAndGradeManagement/src/app/CourseManagement.java
@@ -1,7 +1,6 @@
 package app;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class CourseManagement {
     private static ArrayList<Courses> listOfCourses=new ArrayList<>();
--- Term2-Java(nov-jan_2023_24)/CourseAndGradeManagement/src/app/StudentInfo.java
+++ Term2-Java(nov-jan_2023_24)/CourseAndGradeManagement/src/app/StudentInfo.java
@@ -1,5 +1,4 @@
 package app;
-
 import java.util.ArrayList;
 
 public class StudentInfo {
